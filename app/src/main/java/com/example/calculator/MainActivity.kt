77b5e2379ed5540e5e980aba16563d9865f334fe
KMalif <khafidulmualif2019@gmail.com>
2021-10-05 17:17:52 +0700

    1. Create Calculator Activity

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,59 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        tambahAngka()
+        kurangAngka()
+        kaliAngka()
+        bagiAngka()
+    }
+
+    private fun tambahAngka(){
+        val a = binding.Angka1.text.toString().toInt()
+        val b = binding.Angka2.text.toString().toInt()
+
+        binding.BtnTambah.setOnClickListener {
+            val hasil = a + b
+            binding.EtHasil.setText(hasil)
+        }
+    }
+
+    private fun kurangAngka(){
+        val a = binding.Angka1.text.toString().toInt()
+        val b = binding.Angka2.text.toString().toInt()
+
+        binding.BtnKurang.setOnClickListener {
+            val hasil = a - b
+            binding.EtHasil.setText(hasil)
+        }
+    }
+
+    private fun kaliAngka(){
+        val a = binding.Angka1.text.toString().toInt()
+        val b = binding.Angka2.text.toString().toInt()
+
+        binding.BtnKali.setOnClickListener {
+            val hasil = a * b
+            binding.EtHasil.setText(hasil)
+
+        }
+    }
+
+    private fun bagiAngka(){
+        val a = binding.Angka1.text.toString().toDouble()
+        val b = binding.Angka2.text.toString().toDouble()
+
+        binding.BtnBagi.setOnClickListener {
+            val hasil = a / b
+            binding.EtHasil.setText(hasil.toString())
+        }
+
     }
 }
